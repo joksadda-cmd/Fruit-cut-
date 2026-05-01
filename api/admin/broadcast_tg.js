6f162285264f5c910f7b1e3091cb2a124a009aaa
joksadda-cmd <joksadda@gmail.com>
2026-05-01 09:46:14 +0600

    Update broadcast_tg.js

--- api/admin/broadcast_tg.js
+++ api/admin/broadcast_tg.js
@@ -1,6 +1,6 @@
 const { db } = require('../utils/firebase');
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
